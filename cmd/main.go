4f7a16bb7795682be3641b5d4b9bd93cbc3a72c3
n1k1x86 <i@n1k1x86.ru>
2026-04-15 13:08:27 +1000

    added logger and config

--- cmd/main.go
+++ cmd/main.go
@@ -4,19 +4,34 @@ import (
 	"context"
 	"event_processing_platform/internal/api/routes"
 	"event_processing_platform/internal/api/server"
+	"event_processing_platform/internal/config"
+	zaplogger "event_processing_platform/internal/logger"
 	"log"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"go.uber.org/zap"
 )
 
 func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logger, err := zaplogger.NewLogger(cfg.App.Debug)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Sync()
+
 	app := server.NewHTTPServer()
 	routes.SetRoutes(app)
 	errChan := make(chan error, 1)
 
 	go func() {
-		errChan <- app.Start("localhost:8000")
+		errChan <- app.Start(cfg.HTTPServer.Addr)
 	}()
 
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
@@ -24,17 +39,17 @@ func main() {
 
 	select {
 	case <-ctx.Done():
-		log.Println("shutdown signal received")
+		logger.Info("shutdown signal received")
 		ctxTimeout, cancel := context.WithTimeout(context.Background(), time.Second*5)
 		defer cancel()
 
 		err := app.Stop(ctxTimeout)
 		if err != nil {
-			log.Fatal(err)
+			zaplogger.ExitWithError(logger, "server stopping failed", zap.Error(err))
 		}
 	case err := <-errChan:
 		if err != nil {
-			log.Fatalf("server failed: %s\n", err.Error())
+			zaplogger.ExitWithError(logger, "server failed", zap.Error(err))
 		}
 	}
 }

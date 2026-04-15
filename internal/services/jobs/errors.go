d6d82807d96719860cba1ef0e9de97e5d96b4f8d
n1k1x86 <i@n1k1x86.ru>
2026-04-15 19:17:49 +1000

    added job handler

--- internal/services/jobs/errors.go
+++ internal/services/jobs/errors.go
@@ -11,3 +11,8 @@ var (
 	ErrJobQueueAlreadyExists = errors.New("job queue already exists")
 	ErrJobQueueNotFound      = errors.New("job queue not found")
 )
+
+var (
+	ErrJobHandlerNotFound      = errors.New("job handler not found")
+	ErrJobHandlerAlreadyExists = errors.New("job handler already registered")
+)

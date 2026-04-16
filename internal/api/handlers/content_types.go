5a1607f90eada52efa9ccb548f18c0f84df411ab
n1k1x86 <i@n1k1x86.ru>
2026-04-16 21:30:47 +1000

    added helpers for handlers

---
+++ internal/api/handlers/content_types.go
@@ -0,0 +1,3 @@
+package handlers
+
+var ContentTypeJSON = "application/json"
---
+++ internal/api/handlers/helpers.go
@@ -0,0 +1,51 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gofiber/fiber/v3"
+	"go.uber.org/zap"
+)
+
+type HandlerClientError struct {
+	Details string `json:"details"`
+}
+
+type HandlerServerError struct {
+	Details string `json:"details"`
+}
+
+func handleClientError(c fiber.Ctx, logger *zap.Logger, msg string, err error) {
+	logger.Error(msg, zap.Error(err))
+
+	e := HandlerClientError{
+		Details: msg,
+	}
+	data, err := json.Marshal(e)
+	if err != nil {
+		logger.Error("unexpected error", zap.Error(err))
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	c.Write(data)
+	c.Status(http.StatusForbidden)
+}
+
+func handleServerError(c fiber.Ctx, logger *zap.Logger, msg string, err error) {
+	logger.Error(msg, zap.Error(err))
+
+	e := HandlerServerError{
+		Details: msg,
+	}
+	data, err := json.Marshal(e)
+	if err != nil {
+		logger.Error("unexpected error", zap.Error(err))
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	c.Write(data)
+	c.Status(http.StatusInternalServerError)
+}
